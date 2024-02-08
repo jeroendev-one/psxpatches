257af0ee23e14646b9f51be1736ff35e3e8df36b
Jeroendev <admin@jeroendev.one>
2024-02-08 18:08:54 +0100

    jobs

--- app/Jobs/ProcessPatchesJob.php
+++ app/Jobs/ProcessPatchesJob.php
@@ -37,87 +37,93 @@ class ProcessPatchesJob implements ShouldQueue
      */
     public function handle(): void
     {
+        $chunkSize = 20; // Adjust as needed
+        $totalEntries = Game::where('title_id', 'like', 'CUSA%')->count();
+        
         while (true) {
-            try {
-                Log::channel('patches')->info("Processing range: {$this->start}-{$this->end}");
-            
-                $games = Game::where('title_id', 'like', 'CUSA%')
-                    ->skip($this->start)
-                    ->take($this->end - $this->start + 1)
-                    ->get();
-
-            foreach ($games as $game) {
+            for ($start = 0; $start < $totalEntries; $start += $chunkSize) {
+                $end = min($start + $chunkSize - 1, $totalEntries - 1);
+    
                 try {
-                    sleep(1);
-                    // Make a request to the update XML URL
-                    $response = Http::withoutVerifying()->timeout(60)->get($this->getUpdateXmlUrl($game->title_id));
-            
-                    // Check if the request was successful
-                    if ($response->successful()) {
-                        $xml = simplexml_load_string($response->body());
-                        $xmlVersion = (string)$xml->tag->package['version'];
-                        $xmlContentId = (string)$xml->tag->package['content_id'];
-                        $xmlManifestUrl = (string)$xml->tag->package['manifest_url'];
-                        $xmlSize = (string)$xml->tag->package['size'];
-            
-                        // Check if $game->content_id is not set
-                        if (!$game->content_id) {
-                            // Update $game->content_id if not already set
-                            $game->content_id = $xmlContentId;
-                            $game->save();
-                            Log::channel('patches')->info($game->title_id . ': Content ID updated.');
-                        }
-            
-                        // Check if there are patches for the game
-                        if ($game->patches->isEmpty()) {
-                            // Create a new patch for the game instance
-                            $newPatch = $game->patches()->create([
-                                'version' => $xmlVersion,
-                                'size' => $xmlSize,
-                                'endpoint' => $xmlManifestUrl,
-                            ]);
-            
-                            // Print new version
-                            Log::channel('patches')->info($game->title_id . ': New patch added: ' . $xmlVersion . '!' . PHP_EOL);
-                        } else {
-                            // Get the latest patch by sorting patches by version in descending order
-                            $latestPatch = $game->patches->sortByDesc('version')->first();
-            
-                            // Compare the versions
-                            if (version_compare($xmlVersion, $latestPatch->version, '>')) {
-                                // Update the game with new version and patch size
-                                $game->update([
-                                    'current_version' => $xmlVersion,
-                                    'latest_patch_size' => $xmlSize,
-                                ]);
-            
-                                // Create a new patch for the game instance
-                                $newPatch = $game->patches()->create([
-                                    'version' => $xmlVersion,
-                                    'size' => $xmlSize,
-                                    'endpoint' => $xmlManifestUrl,
-                                ]);
-            
-                                // Print new version
-                                Log::channel('patches')->info($game->title_id . ': New patch added: ' . $xmlVersion . '!' . PHP_EOL);
+                    Log::channel('patches')->info("Processing range: {$start}-{$end}");
+                    
+                    $games = Game::where('title_id', 'like', 'CUSA%')
+                        ->skip($start)
+                        ->take($end - $start + 1)
+                        ->get();
+    
+                    foreach ($games as $game) {
+                        try {
+                            sleep(1);
+                            // Make a request to the update XML URL
+                            $response = Http::withoutVerifying()->timeout(60)->get($this->getUpdateXmlUrl($game->title_id));
+                    
+                            // Check if the request was successful
+                            if ($response->successful()) {
+                                $xml = simplexml_load_string($response->body());
+                                $xmlVersion = (string)$xml->tag->package['version'];
+                                $xmlContentId = (string)$xml->tag->package['content_id'];
+                                $xmlManifestUrl = (string)$xml->tag->package['manifest_url'];
+                                $xmlSize = (string)$xml->tag->package['size'];
+                    
+                                // Check if $game->content_id is not set
+                                if (!$game->content_id) {
+                                    // Update $game->content_id if not already set
+                                    $game->content_id = $xmlContentId;
+                                    $game->save();
+                                    Log::channel('patches')->info($game->title_id . ': Content ID updated.');
+                                }
+                    
+                                // Check if there are patches for the game
+                                if ($game->patches->isEmpty()) {
+                                    // Create a new patch for the game instance
+                                    $newPatch = $game->patches()->create([
+                                        'version' => $xmlVersion,
+                                        'size' => $xmlSize,
+                                        'endpoint' => $xmlManifestUrl,
+                                    ]);
+                    
+                                    // Print new version
+                                    Log::channel('patches')->info($game->title_id . ': New patch added: ' . $xmlVersion . '!' . PHP_EOL);
+                                } else {
+                                    // Get the latest patch by sorting patches by version in descending order
+                                    $latestPatch = $game->patches->sortByDesc('version')->first();
+                    
+                                    // Compare the versions
+                                    if (version_compare($xmlVersion, $latestPatch->version, '>')) {
+                                        // Update the game with new version and patch size
+                                        $game->update([
+                                            'current_version' => $xmlVersion,
+                                            'latest_patch_size' => $xmlSize,
+                                        ]);
+                    
+                                        // Create a new patch for the game instance
+                                        $newPatch = $game->patches()->create([
+                                            'version' => $xmlVersion,
+                                            'size' => $xmlSize,
+                                            'endpoint' => $xmlManifestUrl,
+                                        ]);
+                    
+                                        // Print new version
+                                        Log::channel('patches')->info($game->title_id . ': New patch added: ' . $xmlVersion . '!' . PHP_EOL);
+                                    } else {
+                                        Log::channel('patches')->info($game->title_id . ': No new version.' . PHP_EOL);
+                                    }
+                                }
                             } else {
-                                Log::channel('patches')->info($game->title_id . ': No new version.' . PHP_EOL);
+                                Log::channel('patches')->info($game->title_id . ': Failed to fetch update XML.');
                             }
+                        } catch (Exception $e) {
+                            Log::channel('patches')->error("Error processing game {$game->title_id}: " . $e->getMessage());
                         }
-                    } else {
-                        Log::channel('patches')->info($game->title_id . ': Failed to fetch update XML.');
                     }
                 } catch (Exception $e) {
-                    Log::channel('patches')->error("Error processing game {$game->title_id}: " . $e->getMessage());
+                    Log::channel('patches')->error("Error processing range: {$start}-{$end}: " . $e->getMessage());
                 }
             }
-        
-            Log::channel('patches')->info("Finished processing range: {$this->start}-{$this->end}");
-        } catch (Exception $e) {
-            Log::channel('patches')->error("Error processing range: {$this->start}-{$this->end}: " . $e->getMessage());
+            Log::channel('patches')->info("Finished processing all ranges. Starting over.");
         }
-    }
-}
+    }    
     
     // Helper function to get the update XML URL
     private function getUpdateXmlUrl($titleId)
